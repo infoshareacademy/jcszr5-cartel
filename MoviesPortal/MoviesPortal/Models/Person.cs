072fbb0df9df5c5f865b066e14ddc028818d738e
WTdata <w.tyszkiewicz@areaprojekt.pl>
2022-01-20 17:55:28 +0100

    data models (person, movie, moviestore), IOHelper class - holds data input methods, adding movie via console +adding persons (director, actor)

---
+++ MoviesPortal/MoviesPortal/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿public class Person
+{
+    public string Name { get; set; }   
+
+    public string SurName { get; set; }
+
+    public  Person(string name, string surName)
+    {
+        Name = name;
+        SurName = surName;
+    }
+}
