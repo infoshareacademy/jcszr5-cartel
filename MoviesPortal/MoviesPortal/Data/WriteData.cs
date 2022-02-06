011ff95ddb906a57954abef85cb0321d23d9e472
Mateusz Miga <mateusz.miga11@gmail.com>
2022-02-06 14:34:32 +0100

    Fixed adding users to json file

--- MoviesPortal/MoviesPortal/Data/WriteData.cs
+++ MoviesPortal/MoviesPortal/Data/WriteData.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿//using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace MoviesPortal.Data
 {
@@ -8,10 +9,15 @@ namespace MoviesPortal.Data
         {
             int id = idOld + 1;
             User user = new User(id.ToString(), login, password);
+           
             List<User> users = new List<User>();
-            users.Add(user);
+           
+            string jsonFromFile = File.ReadAllText(@"..\..\..\Database\users.json"); //odczytaj plik z istniejącymi już userami i przekonwertuj do stringa
 
-            var json = JsonConvert.SerializeObject(user, Formatting.Indented);
+            users = JsonSerializer.Deserialize<List<User>>(jsonFromFile); //deserializuj i zapisz istniejących userów do listy       
+            users.Add(user);                                                //aktualizuj listę
+            var writeIndentedOption = new JsonSerializerOptions { WriteIndented = true }; //formatuj plik Json
+            var json = JsonSerializer.Serialize(users, writeIndentedOption); //serializuj zaktualizowaną listę userów
             // to improve
             File.WriteAllText(@"..\..\..\Database\users.json", json);
 
