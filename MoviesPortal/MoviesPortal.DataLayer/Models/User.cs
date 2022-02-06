094b7b15a52b3fe6de0292059d866703f47bf53a
McKormick <dominik_korecki@o2.pl>
2022-02-06 22:39:23 +0100

    user class updated and moved to DataLayer\Models added AdditionalUserService to BusinessLayer AdditionalUserService saves and loads users to/from CSV

---
+++ MoviesPortal/MoviesPortal.DataLayer/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesPortal.DataLayer.Models
+{
+    public enum Role
+    {
+        User,
+        PremiumUser,
+        Admin
+    };
+
+
+    public class User
+    {
+        static int nrOfInstances = 0;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public Role UserRole { get; set; }
+
+        public User()
+        {
+            nrOfInstances++;
+        }
+
+        public User(string name, string password)
+        {
+            Name = name;
+            Password = password;
+            UserRole = Role.User;
+            Id = nrOfInstances;
+            nrOfInstances++;
+        }
+
+        public void GetUserDetails()
+        {
+            Console.WriteLine("ID:{0, -6} Name: {1,-10} Password : {2,-10} Role: {3,-10}", Id, Name, Password, UserRole);
+        }
+    }
+}
