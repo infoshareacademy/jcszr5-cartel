2d15b4ad68525d5891470e52e6516876aa29b226
Mateusz Miga <mateusz.miga11@gmail.com>
2022-02-10 18:26:58 +0100

    update commit

--- MoviesPortal/MoviesPortal/Program.cs
+++ MoviesPortal/MoviesPortal/Program.cs
@@ -4,9 +4,7 @@ using MoviesPortal.BusinessLayer;
 using MoviesPortal.Menu;
 using MoviesPortal.UserService;
 
-IMenu adminMenu = new AdminMenu();
-adminMenu.InitializeMenu();
 
-//LoginPanel loginPanel = new LoginPanel();
-//loginPanel.ChooseOption();
-//MenuInitializer.InitializeMenu();
+LoginPanel loginPanel = new LoginPanel();
+loginPanel.ChooseOption();
+MenuInitializer.InitializeMenu();
