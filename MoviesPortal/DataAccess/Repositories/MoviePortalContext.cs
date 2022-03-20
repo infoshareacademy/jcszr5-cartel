88a8f7604c18b931e08769eb1212c706d41775d8
McKormick <dominik_korecki@o2.pl>
2022-03-20 12:47:59 +0100

    movieportalcontext  class added

---
+++ MoviesPortal/DataAccess/Repositories/MoviePortalContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+
+namespace DataAccess.Repositories
+{
+    public class MoviePortalContext : DbContext
+    {
+
+        public MoviePortalContext() : base("MoviePortalContext")
+        {
+        }
+        public DbSet<DbMovieModel> Movies { get; set; }
+    }
+}
