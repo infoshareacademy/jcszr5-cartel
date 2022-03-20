4d7fa0b57129cbbfff21044d6d3bd0fde283bfad
McKormick <dominik_korecki@o2.pl>
2022-03-20 12:41:02 +0100

    base classes added

---
+++ MoviesPortal/DataAccess/Models/DbCreativePersonModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using DataAccess.Models.Enums;
+
+namespace DataAccess.Models
+{
+    internal class DbCreativePersonModel
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public CreativeRole Role { get; set; }
+
+        public DbCreativePersonModel(string name, string surName, DateTime birthDate, CreativeRole role)
+        {
+            Name = name;
+            SurName = surName;
+            DateOfBirth = birthDate;
+            Role = role;
+        }
+    }
+}
---
+++ MoviesPortal/DataAccess/Models/DbMovieModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models.Enums;
+
+namespace DataAccess.Models
+{
+    public class DbMovieModel
+    {
+        public string Title { get; set; }
+
+        public IList<DbCreativePersonModel> Director = new List<DbCreativePersonModel>();
+
+        public int ProductionYear { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsForKids { get; set; }
+
+
+        public IList<DbCreativePersonModel> ActorList = new List<DbCreativePersonModel>(); 
+
+    }
