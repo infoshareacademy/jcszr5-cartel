519e3f119354acde0acdee77d0b7ec95755e3b36
Mateusz Miga <mateusz.miga11@gmail.com>
2022-04-26 13:29:16 +0200

    TvSeriesAssignationService Added. This class helps to link creatives and genres to TvSeries

---
+++ MoviesPortal/BussinessLogic/Interfaces/ITvSeriesService.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    internal interface ITvSeriesService
+    {
+        public Task CreateNewSeries(TvSeriesModel tvSeries);
+        public Task DeleteSeries(int id);
+        public Task <TvSeriesModel>GetById(int id);
+    }
+}
---
+++ MoviesPortal/BussinessLogic/Services/TvSeriesService.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLogic.Interfaces;
+using DataAccess.Models;
+using DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class TvSeriesService : ITvSeriesService
+    {
+        private readonly ITvSeriesRepository tvSeriesRepository;
+        private readonly ICreativePersonRepository creativePersonRepository;
+        private readonly ISeasonRepository seasonRepository;
+        private readonly IEpisodeRepository episodeRepository;
+        private readonly IGenreRepository genreRepository;
+
+        public TvSeriesService(ITvSeriesRepository tvSeriesRepository, ICreativePersonRepository creativePersonRepository, IGenreRepository genreRepository, IEpisodeRepository episodeRepository, ISeasonRepository seasonRepository)
+        {
+            this.tvSeriesRepository = tvSeriesRepository;
+            this.creativePersonRepository = creativePersonRepository;
+            this.genreRepository = genreRepository;
+            this.episodeRepository = episodeRepository;
+            this.seasonRepository = seasonRepository;
+        }
+
+        public Task CreateNewSeries(TvSeriesModel tvSeries)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteSeries(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TvSeriesModel> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
