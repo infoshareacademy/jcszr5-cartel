c2763f76ae4ba7dd1a5b225a7e79dcb81df4a966
Mateusz Miga <mateusz.miga11@gmail.com>
2022-06-15 12:53:11 +0200

    Added CRUD operations for comments

---
+++ MoviesPortal/DataAccess/Repositories/CommentRepository.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.DbContext;
+using DataAccess.Models;
+using DataAccess.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly MoviePortalContext _context;
+
+        public CommentRepository(MoviePortalContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(CommentModel comment)
+        {
+            await _context.Comments.AddAsync(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var comment =await GetComment(id);
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Edit(int id, CommentModel comment)
+        {
+            var oldComment = await GetComment(id);
+            oldComment.MovieId = comment.MovieId;
+            oldComment.CommentContent = comment.CommentContent;
+            oldComment.UserId = comment.UserId;
+            oldComment.Id = comment.Id;
+            oldComment.PublishedAt = comment.PublishedAt;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IQueryable<CommentModel>> GetAllComments()
+        {
+            var comments = _context.Comments.Include(u => u.ApplicationUser);
+            return comments;
+        }
+
+        public async Task<CommentModel> GetComment(int id)
+        {
+            return await _context.Comments.FirstOrDefaultAsync(c => c.Id == id);
+        }
+    }
+}
---
+++ MoviesPortal/DataAccess/Repositories/Interfaces/ICommentRepository.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Interfaces
+{
+    public interface ICommentRepository
+    {
+        public Task Create(CommentModel comment);
+
+        public Task Delete(int id);
+        
+        Task Edit(int id, CommentModel comment);
+        
+        Task<IQueryable<CommentModel>> GetAllComments();
+        
+        Task<CommentModel> GetComment(int id);
+    }
+}
