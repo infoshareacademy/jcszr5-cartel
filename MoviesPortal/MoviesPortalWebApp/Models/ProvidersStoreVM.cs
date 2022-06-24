5ddfa79eb1e71434528eff2c8c9146bda06911fa
DESKTOP-HM419SK\Movo <lwinowicz@gmail.com>
2022-06-24 09:13:28 +0200

    Update frontend, update details movies

---
+++ MoviesPortal/MoviesPortalWebApp/Models/ProvidersStoreVM.cs
@@ -0,0 +1,13 @@
+﻿namespace MoviesPortalWebApp.Models
+{
+    public class ProvidersStoreVM
+    {
+        public string Link { get; set; }
+
+        public List<ProviderVM> Buy { get; set; }
+
+        public List<ProviderVM> Rent { get; set; }
+
+        public List<ProviderVM> Flatrate { get; set; }
+    }
+}
